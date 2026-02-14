d6da6d87471498ef55dceb17166a66ce7c1fb1f3
siftainejaz <siftainejaz@gmail.com>
2026-02-14 23:52:05 +0530

    Partially implemented FEED API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require('../middlewares/userAuth.js');
 const ConnectionRequest = require('../models/connectionRequest.js');
+const User = require('../models/user.js');
 
 userRouter.get('/connections', userAuth, async (req, res) => {
     try {
@@ -43,7 +44,6 @@ userRouter.get('/connections', userAuth, async (req, res) => {
 
 userRouter.get('/requests/received', userAuth, async (req, res) => {
     try {
-        //toUserId = loggedInUser._id && status = "interested"
         const loggedInUser = req.existingUser;
 
         const connectionRequests = await ConnectionRequest.find(
@@ -65,6 +65,60 @@ userRouter.get('/requests/received', userAuth, async (req, res) => {
         res.status(400).json({
             message: err.message
         })
+    }
+})
+
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+    try {
+        //users whom we have sent connection request
+        //users from whom we have received connection request
+        //users who are a connection
+        //users whom we have rejected
+
+        const loggedInUser = req.existingUser;
+
+        // const connections = await ConnectionRequest.find({
+        //     $or: [
+        //         { fromUserId: loggedInUser._id, status: "accepted" },
+        //         { toUserId: loggedInUser._id, status: "accepted" }
+        //     ]
+        // })
+
+        // const requestReceived = await ConnectionRequest.find({ toUserId: loggedInUser._id, status: "interested" });
+
+        // const requestSent = await ConnectionRequest.find({ fromUserId: loggedInUser._id, status: "interested" });
+
+        // const ignoredUsers = await ConnectionRequest.find({ fromUserId: loggedInUser._id, status: "ignored" });
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+
+
+        const hideUsersFromFeed = new Set();
+
+        connectionRequests.forEach((connectionRequest) => {
+            hideUsersFromFeed.add(connectionRequest.fromUserId.toString());
+            hideUsersFromFeed.add(connectionRequest.toUserId.toString());
+        })
+
+        // console.log(hideUsersFromFeed);
+
+        const users = await User.find({
+            $and: [
+                { _id: { $ne: loggedInUser._id } },
+                { _id: { $nin: Array.from(hideUsersFromFeed) } }
+            ]
+        }).select("firstName lastName age about skills gender");
+
+        res.send(users);
+    }
+    catch (err) {
+        res.send(err.message);
     }
 })
 
